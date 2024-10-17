cbc514835772e64045409a3f2d4fbb76ea99e05f
Alec Theotônio <132586951+alecct812@users.noreply.github.com>
2024-10-17 19:21:51 -0300

    concluding challenge

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "7129", modelo: "Nokia 3310", imei:"818181818", memoria: 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "3918", modelo: "iPhone 16 Pro", imei:"919191919", memoria: 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("YouTube");
